0be1c6194a94eadedef89f10f4c5aaafec111baa
tikum19823 <tikum19823@gmail.com>
2024-06-06 21:42:44 -0600

    Hola mundo

---
+++ package.js
@@ -0,0 +1,10 @@
+{
+    "name": "rest_api",
+    "version": "1.0.0",
+    "descripcion": "Desarrollo Servicios Web",
+    "mai": "index.js",
+    "scripts": {
+        "start": "nodemon app.js"
+    },
+    
+}
